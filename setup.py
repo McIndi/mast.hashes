9b7b29ce724d433118ce019613c2835eb26b8eb6
ilovetux <me@ilovetux.com>
2015-12-02 12:05:11 -0500

    Version Bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "mast.hashes",
-    version = "2.0.0",
+    version = "2.1.0",
     author = "Clifford Bressette",
     author_email = "cliffordbressette@mcindi.com",
     description = ("A library to make it easier to calculate the hash "
